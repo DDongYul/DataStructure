2d3265a1e1aa9e3ee50996f4d18e260be359bc1d
DDongYul <comeme0101@naver.com>
2023-04-09 22:19:00 +0900

    MyNode 메서드 정의

---
+++ src/main/tree/MyNode.java
@@ -0,0 +1,48 @@
+package main.tree;
+
+import java.util.ArrayList;
+
+public class MyNode {
+    private Object element;
+    private MyNode parent;
+    private ArrayList children;
+
+    public MyNode(){
+        element = null;
+        parent = null;
+        children = null;
+    }
+
+    public MyNode(Object element) {
+        this.element = element;
+    }
+
+    public Object element() {
+
+    }
+
+    public MyNode parent() {
+
+    }
+
+    public ArrayList children() {
+
+    }
+
+    public int degree() {
+
+    }
+
+    public void setElement(Object e) {
+
+    }
+
+    public void setParent(MyNode p) {
+
+    }
+
+    public void setChildren(ArrayList c) {
+
+    }
+
+}
