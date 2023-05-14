d980d2b793e9020a952014e5607c5264d660ba37
DDongYul <comeme0101@naver.com>
2023-05-14 23:24:55 +0900

    10주차 실습 구현 목록

---
+++ src/main/map/MyHashTable.java
@@ -0,0 +1,20 @@
+package main.map;
+
+import java.util.ArrayList;
+
+public class MyHashTable {
+    private ArrayList bucketArray;
+    private int bucketCapacity;
+
+    public MyHashTable(int initialCapacity){
+    }
+
+    private int hashFunc(String k) {}
+    public int size() {}
+    public String get(String k) {}
+    public String put(String k, String v){}
+    public String remove(String k){
+
+    }
+
+}
---
+++ src/main/map/StudentInfo.java
@@ -0,0 +1,10 @@
+package main.map;
+
+public class StudentInfo {
+    private String studentID;
+    private String studentName;
+    public void setStudentID(String id) {  }
+    public void setStudentName(String name) {  }
+    public String getStudentID() {  }
+    public String getStudentName() {  }
+}
